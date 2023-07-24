4f7706a7dd3e5b3af8c2a8b45a84f5c09e56ee01
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-07-24 21:19:04 +0700

    Update cloudflared.lua

--- luasrc/model/cbi/cloudflared.lua
+++ luasrc/model/cbi/cloudflared.lua
@@ -7,20 +7,20 @@ a.template = "cloudflared/index"
 t = a:section(NamedSection, "config", "cloudflared")
 t.anonymous = true
 t.addremove = false
-
+---- status
 e = t:option(DummyValue, "status", translate("Status"))
 e.template = "cloudflared/status"
 e.value = translate("Collecting data...")
-
+---- enable
 e = t:option(Flag, "enabled", translate("Enable"))
 e.default = 0
 e.rmempty = false
-
+---- token
 e = t:option(DynamicList, "token", translate('Token'))
 e.password = true
 e.rmempty = false
-
-e = t:option(DummyValue, "opennewwindow", translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Cloudflared Zero Trust\" onclick=\"window.open('https://one.dash.cloudflare.com')\" />"))
+---- cloudflare zero trust
+e = t:option(DummyValue, "opennewwindow", translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Cloudflare Zero Trust\" onclick=\"window.open('https://one.dash.cloudflare.com')\" />"))
 e.description = translate("Create or manage your cloudflare network")
 
 return a
