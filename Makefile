02d124cc7e606860e79d7e0e0624fb20a8edaf49
Hilman Maulana <hilman0.0maulana@gmail.com>
2023-07-24 21:16:21 +0700

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2008-2014 The LuCI Team <luci@lists.subsignal.org>
+#
+# This is free software, licensed under the Apache License, Version 2.0 .
+#
+
+include $(TOPDIR)/rules.mk
+
+PKG_NAME:=luci-app-cloudflared
+PKG_VERSION:=1.0-20232407
+PKG_MAINTAINER:=<https://github.com/animegasan>
+
+LUCI_TITLE:=LuCI for Cloudflared
+LUCI_DEPENDS:=+cloudflared
+LUCI_PKGARCH:=all
+LUCI_DESCRIPTION:=LuCI support for Cloudflared
+
+include $(TOPDIR)/feeds/luci/luci.mk
+
+# call BuildPackage - OpenWrt buildroot signature
---
+++ luasrc/model/cbi/cloudflared.lua
@@ -0,0 +1,26 @@
+local o = require "luci.sys"
+local a, t, e
+a = Map("cloudflared", translate("Cloudflare Zero Trust"))
+a.description = translate("Zero Trust Security services from Cloudflare to help you get maximum security both from outside and within the network.") .. [[<br/><br/><a href="https://github.com/animegasan" target="_blank">Powered by animegasan</a>]]
+a.template = "cloudflared/index"
+
+t = a:section(NamedSection, "config", "cloudflared")
+t.anonymous = true
+t.addremove = false
+
+e = t:option(DummyValue, "status", translate("Status"))
+e.template = "cloudflared/status"
+e.value = translate("Collecting data...")
+
+e = t:option(Flag, "enabled", translate("Enable"))
+e.default = 0
+e.rmempty = false
+
+e = t:option(DynamicList, "token", translate('Token'))
+e.password = true
+e.rmempty = false
+
+e = t:option(DummyValue, "opennewwindow", translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Cloudflared Zero Trust\" onclick=\"window.open('https://one.dash.cloudflare.com')\" />"))
+e.description = translate("Create or manage your cloudflare network")
+
+return a
